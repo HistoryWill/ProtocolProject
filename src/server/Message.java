0825b3f3463fbadb2db678a3a3b11ee038f9f3f4
franguyarmani <fbpeabody@gmail.com>
2017-04-15 10:35:12 -0400

    MessageConstructor(retry)
    This is supposed to be what the last commit was but I forgot to save
    befor the last one

--- src/server/Message.java
+++ src/server/Message.java
@@ -1,8 +1,20 @@
 package server;
 
 import java.io.Serializable;
+import java.time.*;
 
 public class Message implements Serializable{
-	public String[] Recipients;
+	public String[] MessageRecipients;
+	public String MessageBody;
+	public LocalDateTime TimeStamp;
+	
+	public Message(String[] Recipients, String Body){
+		MessageRecipients = Recipients;
+		MessageBody = Body;
+				
+	}
+	
+	
+	
 
 }
